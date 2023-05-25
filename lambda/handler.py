d3ee7d0acc4889e1cb96daec1ae2ad94d2bb1018
Adam Dumicich <adam.dumicich@rmit.edu.au>
2023-05-25 13:30:24 +1000

    Updated lambda code to use boto to upload to s3

--- lambda/handler.py
+++ lambda/handler.py
@@ -1,8 +1,9 @@
 import os
 import requests
 import pandas as pd
-import pyarrow.parquet as pq
 import pyarrow as pa
+import pyarrow.parquet as pq
+import boto3
 from datetime import datetime
 
 # Get the bucket name, url, latitude, and longitude from environment variables
@@ -11,6 +12,7 @@ WEATHER_API_URL = os.getenv('WEATHER_API_URL')
 LATITUDE = os.getenv('LATITUDE')
 LONGITUDE = os.getenv('LONGITUDE')
 
+s3 = boto3.client('s3')
 
 def get_weather_data():
     # Get the url from environment variable and add query parameters
@@ -37,11 +39,22 @@ def lambda_handler(event, context):
     # Define path (folder structure)
     path = f'year={current_time.year}/month={current_time.month}/day={current_time.day}/hour={current_time.hour}/'
 
-    # Convert DataFrame to parquet and save to S3
+    # Convert DataFrame to Parquet
+    table = pa.Table.from_pandas(df)
+
+    # Create an in-memory buffer to write the Parquet data
+    buffer = pa.BufferOutputStream()
+
+    # Write the Parquet data to the buffer
+    pq.write_table(table, buffer)
+
+    # Save the buffer contents to S3
     try:
-        # Create a parquet table from DataFrame and write to S3
-        table = pa.Table.from_pandas(df)
-        pq.write_to_dataset(table, f's3://{BUCKET_NAME}/{path}', compression='snappy')
+        response = s3.put_object(
+            Body=buffer.getvalue().to_pybytes(),
+            Bucket=BUCKET_NAME,
+            Key=path + 'data.parquet',
+        )
     except Exception as e:
         return {'statusCode': 500, 'body': f'Error saving data to S3: {str(e)}'}
 
